4b68650669e4c4744c02e438b5d5bf7b3644ad4f
JTHaugseth <97683128+JTHaugseth@users.noreply.github.com>
2024-02-15 15:10:31 +0100

    update home,global, navbar

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -3,49 +3,50 @@ import { usePathname } from 'next/navigation'
 import Link from 'next/link';
 
 export default function Navbar() {
-    const pathname = usePathname()
+    const pathname = usePathname();
 
     return (
         <nav className="bg-gray-800 text-white p-4">
-            <ul className="flex space-x-4">
-                <li>
-                    <Link className={`hover:text-blue-400 ${pathname === '/' ? 'text-blue-300 font-bold' : 'text-gray-300'}`} href="/">
-                        Home
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        className={`hover:text-blue-400 ${pathname === '/applicants' ? 'text-blue-300 font-bold' : 'text-gray-300'}`}
-                        href="/applicants"
-                    >
-                        For jobbsøkere
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        className={`hover:text-blue-400 ${pathname === '/services' ? 'text-blue-300 font-bold' : 'text-gray-300'}`}
-                        href="/services"
-                    >
-                        Tjenester
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        className={`hover:text-blue-400 ${pathname === '/about' ? 'text-blue-300 font-bold' : 'text-gray-300'}`}
-                        href="/about"
-                    >
-                        Om oss
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        className={`hover:text-blue-400 ${pathname === '/contact' ? 'text-blue-300 font-bold' : 'text-gray-300'}`}
-                        href="/contact"
-                    >
-                        Kontakt oss
-                    </Link>
-                </li>
-            </ul>
+            <div className="w-3/5 mx-auto flex justify-between items-center">
+                <Link className={`hover:text-blue-400 ${pathname === '/' ? 'text-blue-300 font-bold' : 'text-gray-300'}`} href="/">
+                    Karriereflyt
+                </Link>
+
+                <ul className="flex space-x-4">
+                    <li>
+                        <Link
+                            className={`hover:text-blue-400 ${pathname === '/applicants' ? 'text-blue-300 font-bold' : 'text-gray-300'}`}
+                            href="/applicants"
+                        >
+                            For jobbsøkere
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            className={`hover:text-blue-400 ${pathname === '/services' ? 'text-blue-300 font-bold' : 'text-gray-300'}`}
+                            href="/services"
+                        >
+                            Tjenester
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            className={`hover:text-blue-400 ${pathname === '/about' ? 'text-blue-300 font-bold' : 'text-gray-300'}`}
+                            href="/about"
+                        >
+                            Om oss
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            className={`hover:text-blue-400 ${pathname === '/contact' ? 'text-blue-300 font-bold' : 'text-gray-300'}`}
+                            href="/contact"
+                        >
+                            Kontakt oss
+                        </Link>
+                    </li>
+                </ul>
+            </div>
         </nav>
-    )
+    );
 }
