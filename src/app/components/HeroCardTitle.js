20eefff2d06b0b4ed4add38578af70ac06f7a5bb
Jonas Stafset <jonaswstafset@gmail.com>
2024-04-03 09:56:30 +0200

    Endrbare komponenter

--- src/app/components/HeroCardTitle.js
+++ src/app/components/HeroCardTitle.js
@@ -1,8 +1,12 @@
 export default function HeroCardTitle(props) {
   return (
     <>
-      <h1 className="text-4xl font-bold text-gray-800 mb-4">{props.heading}</h1>
-      <p className="text-xl text-gray-600 mb-8">{props.subtitle}</p>
+      <h1 className={props.headingSize + " text-gray-600 mb-8"}>
+        {props.heading}
+      </h1>
+      <p className={props.subtitleSize + " text-xl text-gray-600 mb-8"}>
+        {props.subtitle}
+      </p>
     </>
   );
 }
