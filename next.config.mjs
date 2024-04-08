45afe26c759064d70cd4c53f1d2f7dc9dfef54fc
AugustElvevold <89490288+AugustElvevold@users.noreply.github.com>
2024-04-08 11:27:11 +0200

    Working on cards and images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'onedrive.live.com',
+                pathname: '/**',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
