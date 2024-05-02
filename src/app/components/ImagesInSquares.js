cc2c28ffda6b7034b17edda78695e09b528526c0
Jonas Stafset <jonaswstafset@gmail.com>
2024-05-02 09:20:13 +0200

    squares fix

--- src/app/components/ImagesInSquares.js
+++ src/app/components/ImagesInSquares.js
@@ -2,24 +2,24 @@ import { Squada_One } from "next/font/google";
 
 export default function ImagesInSaquares() {
   return (
-    <div className="md:h-0 h-[500px]">
+    <div className="md:h-0 h-[300px] flex flex-row justify-center gap-0 mt-12">
       <div
         id="squareOne"
-        className="rounded-connie shadow-2xl imagesInSquares lg:tra"
+        className="rounded-connie shadow-2xl imagesInSquares "
       >
-        <div className="imageInSquareOne"></div>
+        1<div className="imageInSquareOne"></div>
       </div>
       <div
         className="rounded-connie  shadow-2xl imagesInSquares "
         id="squareTwo"
       >
-        <div className="imageInSquareTwo"></div>
+        2<div className="imageInSquareTwo"></div>
       </div>
       <div
         className="rounded-connie shadow-2xl imagesInSquares  xl:ml-0 "
         id="squareThree"
       >
-        <div className="imageInSquareThree"></div>
+        3<div className="imageInSquareThree"></div>
       </div>
     </div>
   );
