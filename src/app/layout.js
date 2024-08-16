41bdce9b272742965b5f546790e2358d77aed161
Øyvind Haugen <oyvind@karriereflyt.no>
2024-08-16 10:56:08 +0200

    Lagt inn Google Tag Manager på nytt

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,21 +13,25 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q96T7CX267"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-          function gtag(){
-            dataLayer.push(arguments)
-          }
-        gtag('js', new Date());
-
-        gtag('config', 'G-Q96T7CX267');
-      </script>
         <meta property="og:title" content= "Karriereflyt - Rekruttering & Bemanning"/>
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://www.karriereflyt.no" />
         <meta property="og:description" content="Ta kontakt med oss for bistand!" />
         <meta property="og:image" content="https://karriereflyt.no/logo-slategray.png" />
+
+{/* Google tag (gtag.js) */}
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-Q96T7CX267"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-Q96T7CX267');
+            `,
+          }}
+        />
+
       </head>
       <body className={`${inter.className} eggshell-background`}>
         <BackgroundBoxes />
